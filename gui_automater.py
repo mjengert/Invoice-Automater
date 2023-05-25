822d7cf8a7e312220288c3c7f03ce7aaa18e0706
Maralynn E <m.engert@ufl.edu>
2023-05-25 13:20:41 -0500

    updated gui

--- gui_automater.py
+++ gui_automater.py
@@ -15,6 +15,8 @@ while Invoicemaker:
         invoice = openpyxl.load_workbook(os.path.join(directory, invoices))
         invoice_sheet = invoice.active
         invoice_number = invoice_sheet['B1'].value
+        if invoice_number is None:
+            continue
         for value in invoice_number:
             if value.isdigit():
                 index = invoice_number.index(value)
@@ -25,7 +27,7 @@ while Invoicemaker:
         address = invoice_sheet['B8'].value
         phone_number = invoice_sheet['B9'].value
         invoice_data[name] = {'customer_name': name, 'address': address, 'phone_number': phone_number}
-        Invoicemaker = False
+    Invoicemaker = False
 
 new_invoice = openpyxl.load_workbook('Invoice_template.xlsx')
 new_invoice_sheet = new_invoice.active
@@ -33,7 +35,6 @@ name_list = []
 invoice_info = []
 InvoiceMaker = Tk()
 InvoiceMaker.title("Invoice Maker")
-
 # Set the size of the window
 InvoiceMaker.geometry("550x350")
 
@@ -290,80 +291,3 @@ exit_b = Button(exit_page, text="Exit", foreground='red', font=font2, command=qu
 exit_b.pack(pady=20)
 
 InvoiceMaker.mainloop()
-
-'''class InvoiceMaker(tkinter.Tk):
-    def __init__(self, *args, **kwargs):
-        tkinter.Tk.__init__(self, *args, **kwargs)
-        self.title_font = tkfont.Font(family='Helvetica', size=20, weight='bold', slant='italic')
-        self.smaller_font = tkfont.Font(family='Helvetica', size=12, weight='bold')
-        main_frame = Frame(self)
-        main_frame.pack(side='top', fill='both', expand=True)
-        main_frame.grid_rowconfigure(0, weight=1)
-        main_frame.grid_columnconfigure(0, weight=1)
-        self.first_name = None
-        self.last_name = None
-
-        self.frames = {}
-        for F in (WelcomePage, InvoiceData):
-            page_name = F.__name__
-            frame = F(parent=main_frame, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column=0, sticky='nsew')
-
-        self.show_screen('WelcomePage')
-
-    def show_screen(self, page_name):
-        frame = self.frames[page_name]
-        frame.tkraise()
-
-
-class WelcomePage(tkinter.Frame):
-    def __init__(self, parent, controller):
-        tkinter.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tkinter.Label(self, text='Welcome to the Invoice Maker!', font=controller.title_font)
-        label.pack(side='top', fill='x', pady=20)
-        instruct = tkinter.Label(self, text="Please enter the customer's first and last name:",
-                                 font=controller.smaller_font)
-        instruct.pack(side='top', fill='x', pady=10)
-        f_name = Label(self, text='First Name:')
-        l_name = Label(self, text='Last Name:')
-        f_name.place(x=15, y=130)
-        l_name.place(x=15, y=155)
-        self.first_name_entry = Entry(self, bd=5)
-        self.last_name_entry = Entry(self, bd=5)
-        self.first_name_entry.place(x=95, y=130)
-        self.last_name_entry.place(x=95, y=155)
-        button1 = Label(self, text='')
-        button1.pack(side='right', pady=20)
-        button2 = tkinter.Button(self, text='Submit', command=self.submit_name)
-        button2.place(x=285, y=143.5)
-
-    def submit_name(self):
-        InvoiceData.first_name = self.first_name_entry.get()
-        InvoiceData.last_name = self.last_name_entry.get()
-        self.controller.show_screen('InvoiceData')
-
-
-class InvoiceData(tkinter.Frame):
-    def __init__(self, parent, controller):
-        tkinter.Frame.__init__(self, parent)
-        self.controller = controller
-        self.first_name = self.controller.first_name
-        self.last_name = self.controller.last_name
-        truth = True
-        while truth:
-            print("First Name: ", self.first_name)
-            print("Last Name: ", self.last_name)
-        label = tkinter.Label(self, text='Enter Customer Data', font=controller.title_font)
-        label.pack(side='top', fill='x', pady=10)
-        button1 = tkinter.Button(self, text='Submit', command=quit)
-        button1.pack()
-
-    def __str__(self):
-        return self.controller.first_name + ' ' + self.controller.last_name
-
-
-if __name__ == "__main__":
-    app = InvoiceMaker()
-    app.mainloop()'''
